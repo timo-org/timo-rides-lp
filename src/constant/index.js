a907756296c7493963433480b9d573d1d81137a1
sxhilx <jadasahil786@gmail.com>
2025-07-16 10:43:27 +0200

    chore: add constant folder with navItems array

---
+++ src/constant/index.js
@@ -0,0 +1,6 @@
+export const navItems = [
+  { href: '#', label: 'Home' },
+  { href: '#services', label: 'Services' },
+  { href: '#faqs', label: 'FAQs' },
+  {href: "#contact", label: 'Contact'}
+];
