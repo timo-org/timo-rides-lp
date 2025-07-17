089f0d0304b4b0f6cd4e8bfe90dab651c406c484
sxhilx <jadasahil786@gmail.com>
2025-07-17 15:42:06 +0200

    refactor: rename About section to Why Us across files and assets

--- src/constant/index.js
+++ src/constant/index.js
@@ -1,5 +1,6 @@
 export const navItems = [
   { href: '#', label: 'Home' },
+  { href: '#whyus', label: 'Why us' },
   { href: '#services', label: 'Services' },
   { href: '#faqs', label: 'FAQs' },
   {href: "#contact", label: 'Contact'}
